36c29acf74f75a5a6b804371332c7dffa47e7b18
feng qin <fqin@MacBook-Pro-de-ospiea.local>
2016-09-18 18:19:53 +0200

    1 [Back]administrator can CRUD a user
     Committer: feng qin <feng.qin@ospiea.fr>
     Initial commit

---
+++ src/main/java/fr/ospiea/oscra/common/AbstractEntity.java
@@ -0,0 +1,37 @@
+package fr.ospiea.oscra.common;
+
+import javax.persistence.*;
+import java.util.Date;
+
+/**
+ * Created by fqin on 18/09/2016.
+ */
+@MappedSuperclass
+public abstract class AbstractEntity {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    protected Long id;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "created", nullable = false)
+    private Date created;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "updated", nullable = false)
+    private Date updated;
+
+    @PrePersist
+    protected void onCreate() {
+        updated = created = new Date();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updated = new Date();
+    }
+
+
+
+
+}
