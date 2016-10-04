288a9d711f333d0f5c348253155b488533d7b5df
nicolas2lee <nicolas2lee@gmail.com>
2016-10-04 10:57:34 +0200

    code review version0.1

--- src/main/java/fr/ospiea/oscra/user/service/UserService.java
+++ src/main/java/fr/ospiea/oscra/user/service/UserService.java
@@ -29,8 +29,8 @@ public class UserService {
         return users;
     }
 
-    public User findById(Long employeeId) {
-        return userDao.findOne(employeeId);
+    public User findById(Long userId) {
+        return userDao.findOne(userId);
     }
 
     public User findByUserame(String email) {
@@ -44,7 +44,7 @@ public class UserService {
         return userDao.save(existedUser);
     }
 
-    public void delete(Long employeeId) {
-        userDao.delete(employeeId);
+    public void delete(Long userId) {
+        userDao.delete(userId);
     }
 }
